f7b03ccd40fde32adc1d626ae4aa6adb07f7bed5
MaxiimRandob <madrandob@gmail.com>
2021-08-16 10:25:51 +0300

    spring init

---
+++ src/main/java/com/madrandob/DDBPaginSearch/DdbPaginSearchApplication.java
@@ -0,0 +1,13 @@
+package com.madrandob.DDBPaginSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdbPaginSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdbPaginSearchApplication.class, args);
+	}
+
+}
